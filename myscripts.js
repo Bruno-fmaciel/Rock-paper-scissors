fb598acf1886541f15c5a1ce2aeda45750dd84b7
Bruno-fmaciel <bfma@cesar.school>
2024-02-16 10:50:34 -0300

    Add winner

--- myscripts.js
+++ myscripts.js
@@ -1,3 +1,6 @@
+playerScore = 0;
+computerScore = 0;
+
 function getComputerChoice() {
     lista = ["rock","paper","scissors"]
     randomElement = lista[Math.floor(Math.random() * lista.length)]
@@ -9,21 +12,27 @@ function playRound(player,computer) {
         return "That's a draw! Same choice!"
     }else if (player == "rock") {
         if (computer == "paper") {
+            computerScore++
             return "You lost! Paper beats rock!"
         } else {
+            playerScore++
             return "You win! Rock beats scissors!"
         }
     }else if (player == "paper") {
         if (computer == "rock") {
+            playerScore++
             return "You win! Paper beats rock!"
         }
         else {
+            computerScore++
             return "You lost! Scissors beats paper!"
         }
     }else {
         if (computer == "paper") {
+            playerScore++
             return "You win! Scissor beats paper!"
         } else {
+            computerScore++
             return "You lost! Rock beats scissors!"
         }
     }
@@ -33,8 +42,17 @@ function playGame() {
     for (i = 0;i <= 4;i++) {
         computer = getComputerChoice();
         player = prompt("Choose! Rock, paper or scissors: ").toLowerCase().trim();
-        console.log(playRound(player,computer));
+        console.log(playRound(player,computer));    
+    }
+    console.log(`Player score: ${playerScore}\nComputer score: ${computerScore}`)
+    if (playerScore > computerScore) {
+        console.log("You are the ultimate winner! Congratulations!")
+    }else if (computerScore > playerScore) {
+        console.log("The computer is the ultimate winner! Better luck next time!")   
+    }else {
+        console.log("It's a draw!")
     }
 }
 
 playGame()
+
